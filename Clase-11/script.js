f055a0875513ec8517a11187f7224013b0e6d978
patricio-utn <patricio.utn@gmail.com>
2024-06-07 22:10:28 -0300

    clase 11

--- Clase-11/script.js
+++ Clase-11/script.js
@@ -1,1 +1,14 @@
-console.log 
+/* tipos de datos */
+
+/* DATOS PRIMITIVOS*/
+
+/*SON LOS: 
+Number: ejemplo 8, 0,-2, 3.14 (los con coma son flotantes o float), los NaN, Infinity
+
+String: estos los textos, parrafosm, nombres, van entre comillas. "hola mundo" "pepe"
+
+Boolean: son valores true o false, nos permite representar estados booleanos, hay dos valores posibles. 
+
+Null: ausencia o nulidad de un dato, ejemplo: 
+
+Undefined: no hay un dato definido */
