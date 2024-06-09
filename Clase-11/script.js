b84a0e373c41e90cc37b2b9317cde4c46a687325
patricio-utn <patricio.utn@gmail.com>
2024-06-09 20:38:27 -0300

    clase 11

--- Clase-11/script.js
+++ Clase-11/script.js
@@ -12,3 +12,60 @@ Boolean: son valores true o false, nos permite representar estados booleanos, ha
 Null: ausencia o nulidad de un dato, ejemplo: 
 
 Undefined: no hay un dato definido */
+/* una concatenacion es a cualquier operacion que tenga el + y cualquier string operando, se pueden concatenar textos con numeros */
+/* construcores
+String(dato) transforma  */
+/* TIPOS DE DATOS */
+
+/* Datos objeto: Proximamente */
+
+/* Datos primitivos:
+
+Number => numeros como los conocemos nosotros
+Ej:
+8
+0
+-2
+3.14 (no es decimal, es flotante (float))
+NaN
+Infinity
+
+String => textos, deben ir entre comillas ('', "", ``)
+Ej:
+'hola mundo'
+'pepe'
+'8'
+"pepe"
+`NaN`
+
+
+Boolean => valores true o false
+ej:
+true
+false
+
+Null => ausencia o nulidad de un dato
+ej:
+null
+
+Undefined => no hay un dato definido
+ej:
+undefined
+*/
+
+/* 
+Soy un mensajero que envia una carta, llego a la casa, me atienden y me dicen que NO quieren recibir la carta
+
+estado_de_respuesta: false
+
+
+
+Soy un mensajero que envia una carta, llego a la casa, me atienden y me dicen que quieren recibir la carta
+
+estado_de_respuesta: true
+
+
+
+Soy un mensajero que envia una carta, me extravio en el viaje
+
+estado_de_respuesta: undefined
